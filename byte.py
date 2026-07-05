88620218bef4070ef36e640747a079ba38a3b75d
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-05 17:07:50 +0100

    Add files via upload

--- byte.py
+++ byte.py
@@ -2,16 +2,13 @@ import jpype
 import jpype.imports
 import os
 import copy
-compilers="openjdk-asmtools-jasm $1.jbat -w . "
 print("\033c\033[47;31m\ngive me file  class: to run ? \n")
-#a="Hello.java"
+a="Hello.class"
 a=input().strip()
 b=a.replace(".class","")
-os.system(compilers.replace("$1",b)  )
 if not jpype.isJVMStarted():
     jpype.startJVM(classpath=["."])
 Hello = jpype.JClass(b)
 
-print(Hello.__dir__(""))
-
+print(Hello.class_.__dir__())
 jpype.shutdownJVM()
